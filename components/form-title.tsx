ca0a42a2593e3de0e918f0a0fa7f5e03d8537a5b
Preston Davies <pdavies88@gmail.com>
2024-03-11 12:16:58 -0400

    Editing course fields

---
+++ components/form-title.tsx
@@ -0,0 +1,112 @@
+'use client';
+
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { Pencil } from 'lucide-react';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { toast } from 'sonner';
+
+interface FormTitleProps {
+  initialData: {
+    title: string;
+  };
+  courseId: string;
+}
+
+const formSchema = z.object({
+  title: z.string().min(1, {
+    message: 'Title is required',
+  }),
+});
+
+export const FormTitle = ({ initialData, courseId }: FormTitleProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const toggleEdit = () => setIsEditing((current) => !current);
+
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await fetch(`/api/courses/${courseId}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(values),
+      });
+      toast.success('Course has been updated');
+      toggleEdit();
+      router.refresh();
+    } catch {
+      toast.error('Error occurred while updating course');
+    }
+  };
+
+  return (
+    <div className='mt-6 border bg-slate-100 rounded-md p-4'>
+      <div className='font-medium flex items-center justify-between'>
+        Course title
+        <Button onClick={toggleEdit} variant='ghost'>
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <Pencil className='h-4 w-4 mr-2' />
+              Edit title
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && <p className='text-sm mt-2'>{initialData.title}</p>}
+      {isEditing && (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className='space-y-4 mt-4'>
+            <FormField
+              control={form.control}
+              name='title'
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="e.g. 'Advanced web development'"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className='flex items-center gap-x-2'>
+              <Button disabled={!isValid || isSubmitting} type='submit'>
+                Save
+              </Button>
+            </div>
+          </form>
+        </Form>
+      )}
+    </div>
+  );
+};
