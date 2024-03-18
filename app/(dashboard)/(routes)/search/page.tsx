13466aef957edc2a7c87acb2dfadbedbf8abc813
Preston Davies <pdavies88@gmail.com>
2024-03-17 23:44:22 -0400

    feat: course cards

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,20 +1,44 @@
-import { Categories } from '@/components/categories';
-import { SearchInput } from '@/components/search-input';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 import { db } from '@/lib/db';
+import { SearchInput } from '@/components/search-input';
+import { getCourses } from '@/actions/get-courses';
+import { Categories } from '@/components/categories';
+import { CourseList } from '@/components/course-list';
 
-const SearchPage = async () => {
+interface SearchPageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+}
+
+const SearchPage = async ({ searchParams }: SearchPageProps) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect('/');
+  }
+
   const categories = await db.category.findMany({
     orderBy: {
       name: 'asc',
     },
   });
+
+  const courses = await getCourses({
+    userId,
+    ...searchParams,
+  });
+
   return (
     <>
       <div className='px-6 pt-6 md:hidden md:mb-0 block'>
         <SearchInput />
       </div>
-      <div className='p-6'>
+      <div className='p-6 space-y-4'>
         <Categories items={categories} />
+        <CourseList items={courses} />
       </div>
     </>
   );
