65e3dc240aaf85dcfd8eb0d5c4a57d51515c3751
Preston Davies <pdavies88@gmail.com>
2024-03-19 00:18:34 -0400

    feat: maintainer protected routes

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -2,13 +2,14 @@ import { auth } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
 
 import { db } from '@/lib/db';
+import { isMaintainer } from '@/lib/maintainer';
 
 export async function POST(req: Request) {
   try {
     const { userId } = auth();
     const { title } = await req.json();
 
-    if (!userId) {
+    if (!userId || !isMaintainer(userId)) {
       return new NextResponse('Unauthorized', { status: 401 });
     }
 
