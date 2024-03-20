a9b53f0a46f2375d9be5e908d795da6151836c93
Preston Davies <pdavies88@gmail.com>
2024-03-19 22:16:27 -0400

    feat: extend tiptap

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { type Editor } from '@tiptap/react';
+
 import {
   Bold,
   Strikethrough,
@@ -8,6 +9,17 @@ import {
   List,
   ListOrdered,
   Heading2,
+  Heading1,
+  Heading3,
+  Heading4,
+  Heading5,
+  Heading6,
+  Code,
+  Terminal,
+  Quote,
+  Minus,
+  WrapText,
+  Image as ImageIcon,
 } from 'lucide-react';
 import { Toggle } from '@/components/ui/toggle';
 
@@ -18,8 +30,24 @@ type Props = {
 export const Toolbar = ({ editor }: Props) => {
   if (!editor) return null;
 
+  const addImage = () => {
+    const url = window.prompt('URL');
+
+    if (url) {
+      editor.chain().focus().setImage({ src: url }).run();
+    }
+  };
+
   return (
     <div className='border border-input bg-transparent p-2'>
+      <Toggle
+        size='sm'
+        pressed={editor.isActive('heading')}
+        onPressedChange={() =>
+          editor.chain().focus().toggleHeading({ level: 1 }).run()
+        }>
+        <Heading1 className='h-4 w-4' />
+      </Toggle>
       <Toggle
         size='sm'
         pressed={editor.isActive('heading')}
@@ -27,6 +55,38 @@ export const Toolbar = ({ editor }: Props) => {
           editor.chain().focus().toggleHeading({ level: 2 }).run()
         }>
         <Heading2 className='h-4 w-4' />
+      </Toggle>
+      <Toggle
+        size='sm'
+        pressed={editor.isActive('heading')}
+        onPressedChange={() =>
+          editor.chain().focus().toggleHeading({ level: 3 }).run()
+        }>
+        <Heading3 className='h-4 w-4' />
+      </Toggle>
+      <Toggle
+        size='sm'
+        pressed={editor.isActive('heading')}
+        onPressedChange={() =>
+          editor.chain().focus().toggleHeading({ level: 4 }).run()
+        }>
+        <Heading4 className='h-4 w-4' />
+      </Toggle>
+      <Toggle
+        size='sm'
+        pressed={editor.isActive('heading')}
+        onPressedChange={() =>
+          editor.chain().focus().toggleHeading({ level: 5 }).run()
+        }>
+        <Heading5 className='h-4 w-4' />
+      </Toggle>
+      <Toggle
+        size='sm'
+        pressed={editor.isActive('heading')}
+        onPressedChange={() =>
+          editor.chain().focus().toggleHeading({ level: 6 }).run()
+        }>
+        <Heading6 className='h-4 w-4' />
       </Toggle>
       <Toggle
         size='sm'
@@ -54,17 +114,61 @@ export const Toolbar = ({ editor }: Props) => {
       </Toggle>
       <Toggle
         size='sm'
-        pressed={editor.isActive('bulletList')}
-        onPressedChange={() => editor.chain().focus().toggleBulletList().run()}>
-        <List className='h-4 w-4' />
-      </Toggle>
-      <Toggle
-        size='sm'
         pressed={editor.isActive('orderedList')}
         onPressedChange={() =>
           editor.chain().focus().toggleOrderedList().run()
         }>
         <ListOrdered className='h-4 w-4' />
+      </Toggle>
+      <Toggle
+        size='sm'
+        pressed={editor.isActive('code')}
+        onPressedChange={() => editor.chain().focus().toggleCode().run()}>
+        <Code className='h-4 w-4' />
+      </Toggle>
+      <Toggle
+        size='sm'
+        pressed={editor.isActive('codeBlock')}
+        onPressedChange={() => editor.chain().focus().toggleCodeBlock().run()}>
+        <Terminal className='h-4 w-4' />
+      </Toggle>
+      <Toggle
+        size='sm'
+        pressed={editor.isActive('blockquote')}
+        onPressedChange={() => editor.chain().focus().toggleBlockquote().run()}>
+        <Quote className='h-4 w-4' />
+      </Toggle>
+      <Toggle
+        size='sm'
+        onPressedChange={() =>
+          editor.chain().focus().setHorizontalRule().run()
+        }>
+        <Minus className='h-4 w-4' />
+      </Toggle>
+      <Toggle
+        size='sm'
+        onPressedChange={() => editor.chain().focus().setHardBreak().run()}>
+        <WrapText className='h-4 w-4' />
+      </Toggle>
+      <Toggle size='sm' onPressedChange={addImage}>
+        <ImageIcon className='h-4 w-4' />
+      </Toggle>
+      <Toggle
+        size='sm'
+        onPressedChange={() => editor.chain().focus().undo().run()}
+        disabled={!editor.can().chain().focus().undo().run()}>
+        Undo
+      </Toggle>
+      <Toggle
+        size='sm'
+        onPressedChange={() => editor.chain().focus().redo().run()}
+        disabled={!editor.can().chain().focus().redo().run()}>
+        Redo
+      </Toggle>
+      <Toggle
+        size='sm'
+        onPressedChange={() => editor.chain().focus().clearNodes().run()}>
+        Clear
       </Toggle>
     </div>
   );
